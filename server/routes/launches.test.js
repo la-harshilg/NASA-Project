2e619f9017d859bc3bf5f965a4f4fd2cfd68adee
Harshil Gorasia <gorasiaharshil8@gmail.com>
2022-12-27 09:43:22 +0530

    Updated WOrkflows and Tests

--- server/routes/launches.test.js
+++ server/routes/launches.test.js
@@ -1,51 +1,62 @@
 const request = require("supertest");
 const app = require("../app");
+const { mongoConnect, mongoDisconnect } = require("../services/mongo");
 
-describe("Testing GET /launches", () => {
-  test("GET /launches should return 200.", async () => {
-    const response = await request(app).get("/launches");
-    expect(response.statusCode).toBe(200);
-  });
-});
-
-describe("Testing POST /launches", () => {
-  const completeLaunchDate = {
-    mission: "IND ISRO 1",
-    rocket: "Chandrayan 1",
-    target: "Kepler-442 b",
-    launchDate: "July 22, 2025",
-  };
-
-  const launchDatawithoutdate = {
-    mission: "IND ISRO 1",
-    rocket: "Chandrayan 1",
-    target: "Kepler-442 b",
-  };
-
-  test("POST /launches should return 201.", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(completeLaunchDate)
-      .expect("Content-type", /json/)
-      .expect(201);
-    // expect(response.statusCode).toBe(201);
-
-    const requestDate = new Date(completeLaunchDate.launchDate).valueOf();
-    const responseDate = new Date(response.body.launchDate).valueOf();
-    expect(responseDate).toBe(requestDate);
-
-    expect(response.body).toMatchObject(launchDatawithoutdate);
+describe("Launches API", () => {
+  beforeAll(() => {
+    mongoConnect();
   });
 
-  test("It should catch the missing properties.", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDatawithoutdate)
-      .expect("Content-type", /json/)
-      .expect(400);
+  afterAll(() => {
+    mongoDisconnect();
+  });
 
-    expect(response.body).toStrictEqual({
-      error: "Missing required launch property",
+  describe("Testing GET /launches", () => {
+    test("GET /launches should return 200.", async () => {
+      const response = await request(app).get("/launches");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe("Testing POST /launches", () => {
+    const completeLaunchDate = {
+      mission: "IND ISRO 1",
+      rocket: "Chandrayan 1",
+      target: "Kepler-442 b",
+      launchDate: "July 22, 2025",
+    };
+
+    const launchDatawithoutdate = {
+      mission: "IND ISRO 1",
+      rocket: "Chandrayan 1",
+      target: "Kepler-442 b",
+    };
+
+    test("POST /launches should return 201.", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(completeLaunchDate)
+        .expect("Content-type", /json/)
+        .expect(201);
+      // expect(response.statusCode).toBe(201);
+
+      const requestDate = new Date(completeLaunchDate.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(responseDate).toBe(requestDate);
+
+      expect(response.body).toMatchObject(launchDatawithoutdate);
+    });
+
+    test("It should catch the missing properties.", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDatawithoutdate)
+        .expect("Content-type", /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Missing required launch property",
+      });
     });
   });
 });
